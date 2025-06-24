6f2901b9858405c98b096bc846617b288b1be2e3
Alexandr K. <wildwalker.free@mail.ru>
2025-06-24 09:48:02 +0500

    add: response user as json

--- app/src/Controller/RegisterController.php
+++ app/src/Controller/RegisterController.php
@@ -5,29 +5,29 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Exception\BaseValidationException;
+use App\Responder\JsonResponder;
 use App\Service\RegisterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 
 class RegisterController extends AbstractController
 {
     /**
      * @throws BaseValidationException
+     * @throws ExceptionInterface
      */
     #[Route('/api/register', methods: ['POST'])]
     public function register(
         Request $request,
         RegisterService $registerService,
+        JsonResponder $responder,
     ): Response {
         $requestData = json_decode($request->getContent(), true);
         $user = $registerService->register($requestData);
 
-        return new JsonResponse([
-            'message' => 'User created!',
-            'user' => $user,
-        ], Response::HTTP_CREATED);
+        return $responder->respond($user, Response::HTTP_CREATED);
     }
 }
---
+++ app/src/Responder/JsonResponder.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Responder;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+readonly class JsonResponder
+{
+    public function __construct(private SerializerInterface&NormalizerInterface $serializer)
+    {
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function respond(
+        array|object $data,
+        int $status = Response::HTTP_OK,
+        array $headers = [],
+        array $context = [],
+    ): JsonResponse {
+        $normalized = $this->serializer->normalize(
+            $data,
+            null,
+            array_merge([
+                AbstractNormalizer::IGNORED_ATTRIBUTES => ['passwordHash'],
+            ], $context));
+
+        return new JsonResponse($normalized, $status, $headers);
+    }
+}
